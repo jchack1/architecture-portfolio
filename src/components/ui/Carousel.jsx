90a7dd324c2da3b98276e65c0c5cd8091d4b922f
jchack1 <55161597+jchack1@users.noreply.github.com>
2024-11-15 23:20:10 -0700

    Getting images for carousel

--- src/components/ui/Carousel.jsx
+++ src/components/ui/Carousel.jsx
@@ -1,9 +1,39 @@
+import {useEffect, useState} from "react";
 import styled from "styled-components";
 
 const CarouselContainer = styled.div`
   height: 50vh;
   width: 100vw;
 `;
+
+const getRandomImage = (images) => {
+  const randomIndex = Math.floor(Math.random() * (images.length - 1));
+  return images[randomIndex];
+};
+
+const defaultImageURL =
+  "/src/assets/images/alexander-sinn-rOxuWWhLxXU-unsplash.jpg";
+
 export const Carousel = ({images}) => {
-  return <CarouselContainer></CarouselContainer>;
+  const [currentImage, setCurrentImage] = useState(defaultImageURL);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomImage = getRandomImage(images);
+
+      if (!randomImage) {
+        setCurrentImage(defaultImageURL);
+      } else {
+        setCurrentImage(randomImage);
+      }
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentImage]);
+
+  return (
+    <CarouselContainer>
+      <img src={currentImage}></img>
+    </CarouselContainer>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,21 @@
+import {useEffect, useState} from "react";
+import {Carousel} from "../components/ui/Carousel";
+
+const imageFiles = import.meta.glob("../assets/images/*.{jpg,jpeg,png,gif}"); //too slow, need faster way
+
 export const Home = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const imagePromises = Object.values(imageFiles).map(
+      (importFn) => importFn().then((module) => module.default) // module.default contains the resolved URL
+    );
+
+    Promise.all(imagePromises).then(setImages);
+  }, []);
   return (
     <>
+      <Carousel images={images} />
       <p>
         My name is Lyja and I'm a sad, sad architectural technology student.
       </p>
